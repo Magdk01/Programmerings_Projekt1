5693cfba4e85dd9943e2671bd734db462e6cee59
Jacob pc <jacob.bore>
2021-11-04 15:35:37 +0100

    ikke en ski

--- dataStatistics.py
+++ dataStatistics.py
@@ -1,4 +1,14 @@
 
+import numpy as np
+from dataLoad import dataLoad
+data=dataLoad("test.txt")
 
-def dataStatistics():
-    return None
+def dataStatistics(data, Statistics):
+
+
+    results = data
+    middeltemperatur = np.mean(data[::,0])
+    print(middeltemperatur)
+    return results
+
+print(dataStatistics(data,"statistics"))
