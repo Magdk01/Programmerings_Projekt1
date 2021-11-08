00e32e124352256a0354616fbddbba1a59cc18f4
Magdk01 <Magnusvinjebo@hotmail.com>
2021-11-08 14:08:55 +0100

    Minor fix of plot and setup of Stats

--- dataStatistics.py
+++ dataStatistics.py
@@ -13,8 +13,6 @@ def dataStatistics(data, statistics):
     stdGrowthrate = np.std(data[::,1])
     rows = np.shape(data)[0]
 
-
-
     type = input("Which statistic do you want?"
                  "Type 1 for mean temperature."
                  "Type 2 for mean Growth rate."
@@ -23,15 +21,16 @@ def dataStatistics(data, statistics):
                  "Type 5 for rows."
                  "Type 6 for mean cold growth rate."
                  "Type 7 for mean hot growth rate.")
-    if type = "1":
+
+    if type == "1":
         statistics = meanTemperature
-    elif type = "2":
+    elif type == "2":
         statistics = meanGrowthrate
-    elif type = "3":
+    elif type == "3":
         statistics = stdTemperature
-    elif type = "4":
+    elif type == "4":
         statistics = stdGrowthrate
-    elif type = "5":
+    elif type == "5":
         statistics = rows
     else:
         print("Wrong type of input")
